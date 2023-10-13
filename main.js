d2dfa83e005b83a7ae3b07d56adaebe5b778ec52
fidihartato.pidi@gmail.com <fidihartato.pidi@gmail.com>
2023-10-13 16:33:13 +0700

    menambah kan fitur mengganti tempat penyimpanan dan mengubah beberapa elemen

--- main.js
+++ main.js
@@ -2,11 +2,82 @@ document.querySelector(".btn-reset").addEventListener("click", () => {
   run()
 })
 
-function run(){
+function run() {
   setTimeout(() => {
     document.querySelector("#appVersion").value = navigator.appVersion
   }, 500)
 }
 
+/*
+let kirim = document.querySelector(".kirim")
+
+kirim.addEventListener("click", () => {
+  kirim.parentElement.querySelector("input").innerHTML = ""
+    kirim.parentElement.querySelector("input").innerHTML = ""
+})
+*/
+
 let appVersion = document.querySelector("#appVersion").value = navigator.appVersion
 
+fetch('https://script.google.com/macros/s/AKfycbzheao91uG250fTXCC5P9Ke3cyEk7Mz6bIXiRcsipZ1c6G0QsXhPgENq9j4sL3LtK2IPw/exec')
+  .then(response => response.json())
+  .then(data => {
+
+    let str = `<h3>${data.length} Pesan 📥</h3>`
+    for (let dt of data.reverse()) {
+      str += `
+      <div class="card mt-2">
+        <div class="card-header">
+          @${dt[1]}
+        </div>
+        <div class="card-body">
+          <p class="card-text">${dt[2]}</p>
+        <footer class="mt-3 blockquote-footer">Tanggal: <cite title="Source Title">${ubahTanggal(dt[0])}</cite></footer>
+        </div>
+      </div>
+      `
+    }
+
+
+    document.querySelector(".boxs").innerHTML = str
+
+  })
+  .catch(error => console.error(error));
+
+
+function hapus() {
+  alert("huh... np?")
+}
+
+function ubahTanggal(iso8601) {
+  const date = new Date(iso8601);
+
+  // Mendapatkan komponen tanggal
+  const tahun = date.getFullYear();
+  const bulan = date.getMonth() + 1; // Perlu ditambahkan 1 karena bulan dimulai dari 0 (Januari).
+  const hari = date.getDate();
+
+  // Mendapatkan komponen waktu
+  const jam = date.getHours();
+  const menit = date.getMinutes();
+  const detik = date.getSeconds();
+
+  // Membuat format tanggal dan waktu
+  const formatted = `${tahun}-${bulan}-${hari} | ${jam}:${menit}`;
+
+  return formatted
+}
+
+/*
+<div class="card">
+  <div class="card-header">
+    Featured
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+*/
